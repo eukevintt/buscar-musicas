205b78c95bb9b3743f5233349fbd77059699aa7a
Kevin Araujo <kevinaraujoo677@gmail.com>
2020-05-14 20:39:11 -0300

    Fazendo mudanças no DOM e colocando a lista de músicas em JSON

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
+// Pega no HTML todas as tags e armazena em uma const
 const form = document.querySelector('#form')
 const inputBuscar = document.querySelector('#search')
 const containerMusicas = document.querySelector('#songs-container')
 const paginacao = document.querySelector('#prev-and-next-container')
 
-
 const apiURL = `https://api.lyrics.ovh`
 
+const fetchMusicas = async term => {
+    const response = await fetch(`${apiURL}/suggest/${term}`) //Vai pedir a requisição para a API e como estou usando o async await, enquanto a requisição estiver sendo feita os códigos abaixo não serão executados e o valor não é enviado para a const
+    const data = await response.json() //Transforma a busca em lista de informações da músicas em JSON
+}
+
+//Pega o evento submit do botão e cria uma função
+form.addEventListener('submit', event => {
+    event.preventDefault() //Assim que apertar o botão esse código não deixa com que o navegador atualize a página
+
+    const buscaTermo = inputBuscar.value.trim() //Pega o valor do input e joga dentro da variavel, o trim é para que ele desconsidere os espações caso o cliente coloque
+
+    if(!buscaTermo){ //Essa função não deixa o usuário final deixar o campo input vazio, isso poderia ser resolvido de duas formas: Essa que vamos fazer e a outra seria dentro da tag input colocar um parâmetro chamado <input type="text" required>
+        containerMusicas.innerHTML = `<li class="warning-message">Por favor, digite um termo válido</li>`
+        return
+    }
+
+    fetchMusicas(buscaTermo)
+})
