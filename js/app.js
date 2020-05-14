f3cf40313bd1c054e2be5d20ad9c0bcc343b2ddf
Kevin Araujo <kevinaraujoo677@gmail.com>
2020-05-14 18:06:44 -0300

    Adicionando a API de músicas e pegando cada elemento do DOM

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#form')
+const inputBuscar = document.querySelector('#search')
+const containerMusicas = document.querySelector('#songs-container')
+const paginacao = document.querySelector('#prev-and-next-container')
+
+
+const apiURL = `https://api.lyrics.ovh`
+
